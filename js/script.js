dc417253546f5aa9823a87b34dceb99d53239800
luisvega86 <rusonic@gmail.com>
2022-09-17 23:41:01 -0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-let currentDateAndTime = Date();
-
+// let currentDateAndTime = Date();
+const lastModif = Date(document.lastModified);
 // console.log(`Last update ${currentDateAndTime}`);
-
+// console.log(document.lastModified);
+// console.log(Date(document.lastModified));
 const footer = document.querySelector('footer');
 
 // console.log(footer);
 const dateSmallTag = document.createElement('small');
 
-const updateDate = document.createTextNode(`Last update ${currentDateAndTime}`);
+const updateDate = document.createTextNode(`Last update ${lastModif}`);
 
 dateSmallTag.append(updateDate);
 footer.appendChild(dateSmallTag);
